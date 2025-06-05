7e9a6830905522927469dfb1688ded8136d7c5ad
Francisco Welio <fwelio.dev@gmail.com>
2025-06-05 20:32:36 -0300

    Feature/sanduiches teste

--- src/test/java/SanduicheConcretoTest.java
+++ src/test/java/SanduicheConcretoTest.java
@@ -1,5 +1,21 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SanduicheConcretoTest {
+    @Test
+    void deveCriarSanduicheComTodosIngredientes() {
+        // Arrange
+        Pao pao = new Pao("Integral");
+        Queijo queijo = new Queijo("Prato");
+        Presunto presunto = new Presunto("Frango");
+        Ovo ovo = new Ovo("Capoeira");
+        Tomate tomate = new Tomate();
 
+        // Act
+        SanduicheConcreto sanduiche = new SanduicheConcreto(pao, tomate, queijo, presunto, ovo);
+
+        // Assert
+        assertNotNull(sanduiche, "O sanduíche não deveria ser null");
+    }
 }
